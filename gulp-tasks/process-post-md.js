a0ddbc2218f2fa0fe8bf093b98d5d0a397fd13cf
Edmond Wang <ywang53@ebay.com>
2019-02-18 19:56:37 +0800

    Be able to generate html file

--- gulp-tasks/process-post-md.js
+++ gulp-tasks/process-post-md.js
@@ -7,16 +7,29 @@ let outputDirPath = "./dist/posts/";
 function processPostMd(plugins) {
   return function() {
     console.log("process post markdown files");
-    fsp.mkdir(outputDirPath).then(() => {
-      fsp.readdir(postDirPath).then(mdFileNames => {
-        mdFileNames.forEach(mdFileName => {
-          fsp.readFile(postDirPath + mdFileName).then(content => {
-            let html = marked(content.toString());
-            fsp.writeFile(outputDirPath, html, err => {});
+    fsp
+      .mkdir(outputDirPath)
+      .then(() => {
+        return fsp.readdir(postDirPath).then(mdFileNames => {
+          mdFileNames.forEach(mdFileName => {
+            fsp.readFile(postDirPath + mdFileName).then(content => {
+              let html = marked(content.toString());
+              fsp
+                .writeFile(
+                  outputDirPath + mdFileName + ".html",
+                  html,
+                  err => {}
+                )
+                .catch(err => {
+                  console.log(err);
+                });
+            });
           });
         });
+      })
+      .catch(err => {
+        console.log(err);
       });
-    });
     return Promise.resolve("process post markdown files");
   };
 }
